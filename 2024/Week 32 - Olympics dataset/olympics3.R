7e658e6c49d3792d9cdc3ffcba7a185eb3866b66
spmeikon <pgeneta@gmail.com>
2024-08-21 11:15:26 +0100

    fix status of files

--- 2024/Week 32 - Olympics dataset/olympics3.R
+++ 2024/Week 32 - Olympics dataset/olympics3.R
@@ -6,29 +6,29 @@ tuesdata <- tt_load(2024, week = 32)
 oly <- tuesdata$olympics
 
 
-df <- oly %>% 
-  select(id, name, noc, year, season, city) %>% 
-  group_by(id, name) %>% 
+df <- oly |> 
+  select(id, name, noc, year, season, city) |> 
+  group_by(id, name) |> 
   unique()
 
 
 vis <- function(data, seas = c("Summer", "Winter")){
-  top_noc <- data %>% 
-    group_by(noc) %>% 
-    summarise(total_count = n()) %>% 
-    ungroup() %>% 
-    top_n(5) %>% 
+  top_noc <- data |> 
+    group_by(noc) |> 
+    summarise(total_count = n()) |> 
+    ungroup() |> 
+    top_n(5) |> 
     pull(noc)
   
-  df <- data %>% 
-    filter(noc %in% top_noc) %>% 
-    group_by(year, noc) %>% 
-    summarise(count = n(), .groups = 'drop') %>% 
-    ungroup() %>% 
-    group_by(noc) %>% 
-    mutate(label = ifelse(row_number() == n(), paste0(noc, " - ", count), NA)) %>% 
-    ungroup() %>% 
-    arrange(year, noc) %>% 
+  df <- data |> 
+    filter(noc %in% top_noc) |> 
+    group_by(year, noc) |> 
+    summarise(count = n(), .groups = 'drop') |> 
+    ungroup() |> 
+    group_by(noc) |> 
+    mutate(label = ifelse(row_number() == n(), paste0(noc, " - ", count), NA)) |> 
+    ungroup() |> 
+    arrange(year, noc) |> 
     ggplot(aes(x = year, y = count, color = noc, label = label))+
     geom_line(alpha = 0.6, linewidth = 1) +
     geom_text(nudge_x = 3, size = 4)+
@@ -47,22 +47,22 @@ vis(oly, 'Winter')
 
 
 #=================
-top_noc <- oly %>% 
-  filter(season == "Winter") %>% 
-  group_by(noc) %>% 
-  summarise(total_count = n()) %>% 
-  ungroup() %>% 
-  top_n(5) %>% 
+top_noc <- oly |> 
+  filter(season == "Winter") |> 
+  group_by(noc) |> 
+  summarise(total_count = n()) |> 
+  ungroup() |> 
+  top_n(5) |> 
   pull(noc)
 
-df <- oly %>% 
-  filter(noc %in% top_noc) %>% 
-  group_by(year, noc) %>% 
-  summarise(count = n(), .groups = 'drop') %>% 
-  ungroup() %>% 
-  group_by(noc) %>% 
-  #mutate(label = ifelse(row_number() == n(), paste0(noc, " - ", count), NA)) %>% 
-  ungroup() %>% 
+df <- oly |> 
+  filter(noc %in% top_noc) |> 
+  group_by(year, noc) |> 
+  summarise(count = n(), .groups = 'drop') |> 
+  ungroup() |> 
+  group_by(noc) |> 
+  #mutate(label = ifelse(row_number() == n(), paste0(noc, " - ", count), NA)) |> 
+  ungroup() |> 
   arrange(year, noc)
 
 plot <- ggplot(df, aes(x = year, y = count, fill = noc) )+
@@ -85,7 +85,7 @@ fig <- plot_ly(df, x =~year,
                stackgroup = 'one',
                hoverinfo = 'text',
                hovertext = paste0("Country: ", df$noc,
-                                  "<br>Count: ", df$count) ) %>% 
+                                  "<br>Count: ", df$count) ) |> 
   layout(title = paste0("Top 5 country participation, Winter Olympics"),
          yaxis = list(title = "Total Athletes"),
          xaxis = list(title = "Year"),
